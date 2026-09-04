a0aa1364473955ffad1d473a54c42670d815b431
Koushik Biswas <162034238+Koushik-Biswas@users.noreply.github.com>
2026-09-05 01:22:28 +0530

    Time: 0 ms (100.00%) | Memory: 46.1 MB (74.88%) - LeetSync

---
+++ 117-populating-next-right-pointers-in-each-node-ii/populating-next-right-pointers-in-each-node-ii.java
@@ -0,0 +1,57 @@
+/*
+// Definition for a Node.
+class Node {
+    public int val;
+    public Node left;
+    public Node right;
+    public Node next;
+
+    public Node() {}
+    
+    public Node(int _val) {
+        val = _val;
+    }
+
+    public Node(int _val, Node _left, Node _right, Node _next) {
+        val = _val;
+        left = _left;
+        right = _right;
+        next = _next;
+    }
+};
+*/
+
+class Solution {
+    public Node connect(Node root) {
+
+        Node curr = root;
+
+        while (curr != null) {
+
+            // Dummy node for the next level
+            Node dummy = new Node(0);
+            Node tail = dummy;
+
+            // Traverse current level using next pointers
+            while (curr != null) {
+
+                if (curr.left != null) {
+                    tail.next = curr.left;
+                    tail = tail.next;
+                }
+
+                if (curr.right != null) {
+                    tail.next = curr.right;
+                    tail = tail.next;
+                }
+
+                curr = curr.next;
+            }
+
+            // Move to the next level
+            curr = dummy.next;
+        }
+
+        return root;
+    }
+}
